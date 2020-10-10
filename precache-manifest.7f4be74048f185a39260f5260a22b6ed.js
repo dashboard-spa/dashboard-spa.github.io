3cbd1d1ff93b0bdf566c914ed291b2ede1d09e24
Mehdi Faraji <mahdifarajideveloper@gmail.com>
2020-10-10 16:36:29 +0330

    commit

---
+++ precache-manifest.7f4be74048f185a39260f5260a22b6ed.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f09d33f5fd5c21553fe10cc9c0c2370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174912ee09e338da8cbb",
+    "url": "/static/css/3.81658e05.chunk.css"
+  },
+  {
+    "revision": "e78f892b077b8957a74b",
+    "url": "/static/css/5.f561cc8b.chunk.css"
+  },
+  {
+    "revision": "e4b891290e5475016130",
+    "url": "/static/css/6.05b070f5.chunk.css"
+  },
+  {
+    "revision": "fa1c9c691313cf2165e4",
+    "url": "/static/css/main.71bc517c.chunk.css"
+  },
+  {
+    "revision": "030d06df607916957955",
+    "url": "/static/js/2.a1682473.chunk.js"
+  },
+  {
+    "revision": "38591027583a72c6681ebffc516006b5",
+    "url": "/static/js/2.a1682473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174912ee09e338da8cbb",
+    "url": "/static/js/3.a7370e47.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.a7370e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb04ae0ccd20069ba80",
+    "url": "/static/js/4.70141bcb.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/static/js/4.70141bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78f892b077b8957a74b",
+    "url": "/static/js/5.32f02fd6.chunk.js"
+  },
+  {
+    "revision": "e4b891290e5475016130",
+    "url": "/static/js/6.6034845c.chunk.js"
+  },
+  {
+    "revision": "fa1c9c691313cf2165e4",
+    "url": "/static/js/main.a5219be2.chunk.js"
+  },
+  {
+    "revision": "6d2a467b0eb90999c9c9",
+    "url": "/static/js/runtime-main.b1bcdca4.js"
+  },
+  {
+    "revision": "b71b78f998792452203d5899c9f6c3a4",
+    "url": "/static/media/admin.b71b78f9.jpg"
+  },
+  {
+    "revision": "12fcb036e5af1857e26fa728fc11350a",
+    "url": "/static/media/facebook.12fcb036.png"
+  },
+  {
+    "revision": "534811d6586c78312036cbcb0f93a091",
+    "url": "/static/media/instagram.534811d6.png"
+  },
+  {
+    "revision": "1196a9173a07800db912760cd795037d",
+    "url": "/static/media/twitter.1196a917.png"
+  }
+]);
